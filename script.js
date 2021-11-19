4651a130f12eb1274e100b4c730941c8bfea7e7b
rvua <rvanore26@gmail.com>
2021-11-19 14:22:35 -0600

    --

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function weatherAlert() {
-    alert("Uploading weather report")
+console.log("page loading...");
+
+var cookieDiv = document.querySelector(".cookie-policy");
+
+function loading() {
+    alert("Loading weather report...")
 }
 
-var cookie = document.querySelector("#main-cookie-div");
-function acceptRemove() {
-    cookie.remove();
+function accept() {
+    cookieDiv.remove();
+}
+
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
 }
